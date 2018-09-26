77fbc55c39b34bb0a763e8123f6a2c64e7d8d428
steve garber <steve.l.garber@gmail.com>
2018-09-27 01:57:56 +0300

    built some parallax stuff with tupac

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,66 @@
 import React, { Component } from "react";
+import { Parallax as Px } from "react-parallax";
+import Tupac from "./imgs/tupac.png";
+import Toopac from "./imgs/2pac.png";
+import crosshair from "./imgs/crosshair.png";
+import LosAng from "./imgs/LA.png";
+import Ak from "./imgs/ak-47.png";
+import discs from "./imgs/discs.png";
 class Home extends Component {
   render() {
     return (
       <div>
+        <div className="Plax-Container">
+          <Px
+            className="Home-Header-Parallax"
+            bgImage={LosAng}
+            strength={500}
+            blur={{ min: -15, max: 20 }}
+          >
+            <img src={Tupac} className="Tupac" />
+          </Px>
+          <Px
+            className="Crosshair"
+            strength={300}
+            bgImage="none"
+            renderLayer={pg => (
+              <img
+                src={crosshair}
+                style={{
+                  position: "fixed",
+                  top: pg * 40 * Math.sin(7 * pg) + 20 + "vh",
+                  left: 100 * pg - 50 + "vw",
+                  display: pg > 0.95 ? "none" : ""
+                }}
+              />
+            )}
+          />
+        </div>
+
         <section className="hero">
           <div
             className="background-image"
             style={{ backgroundImage: "url()" }}
           />
-          <h1>Blank Homepage Component</h1>
-          <h3>Write soome other thing here about this thing.</h3>
-          <a href="..." className="btn">
-            Download it Here
-          </a>
+          <h1>Reppin the best coast beeeyach!</h1>
+          <h3>Write soome other things here about this here thing.</h3>
+          <img src={Ak} className="Ak" />
+          <Px
+            className="discs"
+            strength={250}
+            bgImage="none"
+            renderLayer={pg => (
+              <img
+                src={discs}
+                style={{
+                  position: "fixed",
+                  top: "50vh",
+                  left: 100 * pg + "vw",
+                  display: pg < 0.37 ? "none" : ""
+                }}
+              />
+            )}
+          />
         </section>
 
         <section className="our-work">
