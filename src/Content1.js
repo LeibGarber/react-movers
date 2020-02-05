cd1e96ade638b6ab82489cafc211f734a4bb2f06
leibGarber <steve.l.garber@gmail.com>
2020-02-05 12:39:01 +0200

    forgot to commit oy...wrote home page and faq page content, set pics up for now will need lower res files and other sizes for mobile

--- src/Content1.js
+++ src/Content1.js
@@ -1,26 +1,79 @@
 import React, { Component } from "react";
+import womanHoldingBox from "./imgs/woman-holding-moving-box.jpg";
+import movingTruck from "./imgs/moving-truck-clear-sky-expressway.jpg"
+import places from "./Places"
+
+
 class Content1 extends Component {
   render() {
     return (
-      <div>
+      <div className="MovingFaq">
         <section className="hero">
           <div
             className="background-image"
-            style={{ backgroundImage: "url()" }}
+            style={{ backgroundImage: `url(${womanHoldingBox})` }}
           />
-          <h1>This is a blank content page component</h1>
-          <h3>Write soome other thing here about this thing.</h3>
+        <h1>Moving Faq</h1>
+          <h3>Lets answer the most frequently asked questions about moving</h3>
         </section>
-        <section className="features">
-          <h3 className="title">Some recent work</h3>
-          <p>
-            כל הדברים שבגמרא הבבלי חייבין כל ישראל ללכת בהם וכופין כל עיר ועיר
-            וכל מדינה ומדינה לנהוג בכל המנהגות שנהגו חכמי הגמרא
-          </p>
-          <br />
-          <h1> This is a blank content view component</h1>
-          <br />
-        </section>
+        <section className="our-work">
+          <h2>How can I find movers near me?</h2>
+          <p>Well you are certainly in the right place now! Our team is happy to find you local movers near you who will do a great job and get you into your new home in no time.</p>
+          <spacer></spacer>
+          <h2>How should I pack dishes?</h2>
+          <p>Check out our handy guide on how to pack! Keep in mind our movers can also provide you with a full packing service where we will bring every kind of packing material you could possibly need. Everything from small moving boxes, or medium moving boxes, to the bubble wrap, shrink wrap, or even hanging wardrobe boxes so that your clothes can all stay nice and neat on their hangars and arrive crisp and ready to wear. </p>
+        <spacer></spacer>
+        <h2>Can you ship pianos?</h2>
+        <p>Sure can! We've moved everything from Steinway Grands to the classic Fender Rhodes. We give discounts if you can play Chopins nocturnes :) </p>
+        <spacer></spacer>
+        <h2>Do you handle auto shipping?</h2>
+        <p>Absolutely! We work with the nations top auto shipping companies and can offer you a number of great solutions for shipping your car or shipping motorcycles. Check out more information on auto shipping here. </p>
+        <spacer></spacer>
+        <h2>Whats a binding quote? Or a binding moving estimate?</h2>
+        <p> A binding quote is the contract which licensed moving companies use to make a formal agreement to fulfill your move. The way these quotes work is that we say based on this amount of stuff, we can all agree that the bill is this amount per unit.  Movers charge either by the foot or by the pound for any cross country move, and sometimes also for local moves. Shorter distance local moves are typically billed by the hour. The binding part of this estimate is that the license requires the movers to honor the rate they agree to on the day of the move. This is one of the big advantages of using a licensed moving company as opposed to a broker agency. Unfortunately brokers are notorious for bait and switch tactics and holding peoples items hostage. Licensed moving carriers such as the companies we partner with will honor their rates on move day. Check out Understanding my moving quote </p>
+          <spacer></spacer>
+          <h2> So are you a moving company? Or are you moving brokers?</h2>
+          <p> Actually we are neither. We are like the travel agents of the moving industry. We deal with thousands of moves per month and can therefore get you great deals on your move, and help you find a company which will best fit your needs. We handle the logistical side of building estimates, scheduling moves, and keeping customers happy for a few dozen moving companies around the country. With us, you get the best of both worlds. All of the advantages of working with only licensed moving companies, and all of the advantages while also having the advanage of multiple options so that you can get the best deal for your move. </p>
+            <h2>I have a relocation package, can you work with my HR Department</h2>
+
+            <p>First of all congratulations on having such an amazing job that they are taking care of you like this! We handle thousands of moves every month, and its rare for a compnay offer a relocation package which will cover the cost of hiring a moving company for your cross country move. That being said, we work with HR Departments all the time and are happy to accomodate. In our experience what most HR Departments require in order to pay for your relocation is to have 3 or 4 binding estimates. Nine times out of ten your company will want to pay for the lowest priced quote you submit. Keep in mind that once they approve a price they will not want to pay more so it's really important to get an accurate quote which leaves some buffer in case there are any last minute additions.</p>
+
+      </section>
+      <section className="hero">
+        <div
+          className="background-image"
+          style={{ backgroundImage: `url(${movingTruck})` }}
+        />
+      <h1>Ready to hit the road?</h1>
+    </section>
+
+      <section className="reviews">
+        <h3 className="title">What others say:</h3>
+
+        <p className="quote">
+        Wow I can't believe what a great job these guys did. My only problem? Why did I do so many moves myself when I could have just hired Best Movers to do it for me?
+        </p>
+        <p className="author">— Bob Smith</p>
+
+        <p className="quote">
+          I was in a jam and Best Movers saved the day. Had to move my mother after my father died. She was alone, miserable and dreading the move. Best Movers came in, packed the whole house up in a day, and got all of my mothers stuff out to us in no time. They were so nice to her and really helped my family through a time and helped us get our best foot forward. Love you guys
+        </p>
+        <p className="author">— Jimmy Cohn</p>
+
+        <p className="quote">
+          Best Movers work like gangbusters. We needed my entire startup moved to our new location across town like yesterday and I had no idea how to get this sorted. Comes Best Movers to pack up 30 workstations, cubicles, desks, office supplies and get my entire staff set up the next day. Did such a great job on that one that I've now used them a dozen times to move my team all around the country. These guys really are the best movers Coast to Coast for both local movers, or cross country movers. A team you can trust for any job. Highly recommend.
+        </p>
+        <p className="author">— Gary V</p>
+      </section>
+
+        <h1> Do you have a moving company near me?</h1>
+
+        <h2> </h2>
+        {places.map( (place, i)=> (<h2 key={i}>Do you have movers in {place}?</h2>)) }
+
+{/*try using flatmap to work thru different sentences here for different h2s questions.map or questions.flatmap*/}
+
+
       </div>
     );
   }
--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,7 @@ import Tupac from "./imgs/tupac.png";
 import Toopac from "./imgs/2pac.png";
 import crosshair from "./imgs/crosshair.png";
 import LosAng from "./imgs/LA.png";
+import whiteTruck from "./imgs/white-moving-truck-near-pine-trees.png"
 import Ak from "./imgs/ak-47.png";
 import discs from "./imgs/discs.png";
 class Home extends Component {
@@ -13,28 +14,22 @@ class Home extends Component {
         <div className="Plax-Container">
           <Px
             className="Home-Header-Parallax"
-            bgImage={LosAng}
+            bgImage={whiteTruck}
+            bgImageAlt="movers with white cross country moving truck doing long distance move in front of pine trees and hills"
             strength={500}
             blur={{ min: -15, max: 20 }}
+
           >
-            <img src={Tupac} className="Tupac" />
+            <div className="Plax-Text">
+              <div className="text-spacer"></div>
+              <div><h1 >Best Movers gets moving companies near you</h1></div>
+              <h3>Get 100% free moving quotes from cross country movers. </h3>
+              <h3>Need long distance movers? Get 3 quotes right now. </h3> <h3>Want local movers? We can help as easy as 1 2 3!</h3>
+              <div className="text-spacer"></div>
+
+            </div>
           </Px>
-          <Px
-            className="Crosshair"
-            strength={300}
-            bgImage="none"
-            renderLayer={pg => (
-              <img
-                src={crosshair}
-                style={{
-                  position: "fixed",
-                  top: pg * 40 * Math.sin(7 * pg) + 20 + "vh",
-                  left: 100 * pg - 50 + "vw",
-                  display: pg > 0.95 ? "none" : ""
-                }}
-              />
-            )}
-          />
+
         </div>
 
         <section className="hero">
@@ -42,32 +37,28 @@ class Home extends Component {
             className="background-image"
             style={{ backgroundImage: "url()" }}
           />
-          <h1>Reppin the best coast beeeyach!</h1>
-          <h3>Write soome other things here about this here thing.</h3>
-          <img src={Ak} className="Ak" />
-          <Px
-            className="discs"
-            strength={250}
-            bgImage="none"
-            renderLayer={pg => (
-              <img
-                src={discs}
-                style={{
-                  position: "fixed",
-                  top: "50vh",
-                  left: 100 * pg + "vw",
-                  display: pg < 0.37 ? "none" : ""
-                }}
-              />
-            )}
-          />
+        <h1> Want long distance movers?</h1>
+
+            <h2>Our long distance moving companies are licesned by the Department of Transportation</h2>
+            <p>We work with dozens of long distance moving companies, each one has all of the paperwork required by the United States Department of Transportation to legally move your items. Its very important to only hire a licensed moving carrier when planning a long distance move. Find out more about the difference between licensed moving companies as opposed to moving brokers. </p>
+        <h2>Professional movers </h2>
+            <p>Our moving companies only hire proessional movers who have passed background checks and drug testing. You can expect kind and courteous movers who will treat your items with the same care you would. </p>
+            <h2>A large fleet of the newest moving trucks </h2>
+            <p>Our moving trucks are modern moving trucks with air ride suspension which reduces the amount of vibrations and therefore keeps your household goods safe for the long trip cross country. Our moving trucks have clean maintenance records, and spotless trailers so you can relax and know that your items will be delivered in the exact same shape they were when they went on the moving truck.</p>
+            <h2>Considering UHaul truck rental?</h2>
+            <p>Forget about difficult options with lots of hidden costs like UHaul Truck Rental, and let us do the packing and loading for you.</p>
+
+
+
+
+          <div>  <h2>Get a free moving quote now!</h2></div>
+
         </section>
 
         <section className="our-work">
-          <h3 className="title">Some recent work</h3>
+          <h3 className="title">Googling how to prepare for your move?</h3>
           <p>
-            כל הדברים שבגמרא הבבלי חייבין כל ישראל ללכת בהם וכופין כל עיר ועיר
-            וכל מדינה ומדינה לנהוג בכל המנהגות שנהגו חכמי הגמרא
+            Check out our blog posts and get some good ideas about how to decide between movers, how to pack, and all the other moving related questions you may have.
           </p>
           <br />
 
@@ -82,34 +73,30 @@ class Home extends Component {
         <section className="features">
           <h3 className="title">Features and services</h3>
           <p>
-            כל הדברים שבגמרא הבבלי חייבין כל ישראל ללכת בהם וכופין כל עיר ועיר
-            וכל מדינה ומדינה לנהוג בכל המנהגות שנהגו חכמי הגמרא
+            What is it exactly that a full service moving company does?
           </p>
           <br />
 
           <ul className="grid">
             <li>
               <i className="fa fa-camera-retro" />
-              <h4>Photography</h4>
+              <h4>Loading and unloading</h4>
               <p>
-                כל הדברים שבגמרא הבבלי חייבין כל ישראל ללכת בהם וכופין כל עיר
-                ועיר וכל מדינה ומדינה לנהוג בכל המנהגות שנהגו חכמי הגמרא
+                Included with every move is a full loading and unloading service. Depending on the size of your move, we will send between 2-6 professional movers to handle all of the labor neccessary to get your move taken care of. Our men will load all of the things you need to have shipped, and stack them neatly on our truck ensuring that everything will stay in place for the long journey to your new home. On the day of the delivery, one of our professional moving teams will also unload every last item off of the truck and get your entire house set up so you can relax, and get cozy in your new home that same day.
               </p>
             </li>
             <li>
               <i className="fa fa-cubes" />
-              <h4>Web Development</h4>
+              <h4>Disassembly and reassembly</h4>
               <p>
-                כל הדברים שבגמרא הבבלי חייבין כל ישראל ללכת בהם וכופין כל עיר
-                ועיר וכל מדינה ומדינה לנהוג בכל המנהגות שנהגו חכמי הגמרא
+               Any items which will be easier to move, or which need to be taken apart to move safely or even to make it down the stairs or out the front door our movers will dissaemble with care. All the little nuts and bolts and screws will be bagged in ziplocs so that every single washer will be there when we get to your new home and reassemble all of your furniture.
               </p>
             </li>
             <li>
               <i className="fa fa-newspaper-o" />
-              <h4>Content Editing</h4>
+              <h4>Basic Packing</h4>
               <p>
-                כל הדברים שבגמרא הבבלי חייבין כל ישראל ללכת בהם וכופין כל עיר
-                ועיר וכל מדינה ומדינה לנהוג בכל המנהגות שנהגו חכמי הגמרא
+                Included with every long distance move, or local move is a basic packing service. Our movers will wrap all of your snug as a bug in a rug in moving blankets and padded moving quilts before loading them onto our moving trucks. Any other packing materials you may need we will have on the truck just in case :)
               </p>
             </li>
           </ul>
@@ -119,23 +106,19 @@ class Home extends Component {
           <h3 className="title">What others say:</h3>
 
           <p className="quote">
-            Have you ever noticed that anybody driving slower than you is an
-            idiot, and anyone going faster than you is a maniac?
+          Wow I can't believe what a great job these guys did. My only problem? Why did I do so many moves myself when I could have just hired Best Movers to do it for me?
           </p>
-          <p className="author">— George Carlin</p>
+          <p className="author">— Bob Smith</p>
 
           <p className="quote">
-            'Cause I'm leavin' on a jet plane Don't know when I'll be back again
-            Oh babe, I hate to go
+            I was in a jam and Best Movers saved the day. Had to move my mother after my father died. She was alone, miserable and dreading the move. Best Movers came in, packed the whole house up in a day, and got all of my mothers stuff out to us in no time. They were so nice to her and really helped my family through a time and helped us get our best foot forward. Love you guys
           </p>
-          <p className="author">— John Denver</p>
+          <p className="author">— Jimmy Cohn</p>
 
           <p className="quote">
-            I'm not so mean. I wouldn't ever set out to hurt anyone deliberately
-            unless it was, you know, important - like a league game or
-            something.{" "}
+            Best Movers work like gangbusters. We needed my entire startup moved to our new location across town like yesterday and I had no idea how to get this sorted. Comes Best Movers to pack up 30 workstations, cubicles, desks, office supplies and get my entire staff set up the next day. Did such a great job on that one that I've now used them a dozen times to move my team all around the country. These guys really are the best movers Coast to Coast for both local movers, or cross country movers. A team you can trust for any job. Highly recommend.
           </p>
-          <p className="author">— Dick Butkiss</p>
+          <p className="author">— Gary V</p>
         </section>
 
         <section className="contact">
